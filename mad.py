917aaeacd95bc42c33dfa14b4ef0cab15a9a0e31
aeonincode <khashayar.sasannia@hotmail.com>
2020-04-23 10:35:15 +0200

    first commit

---
+++ mad.py
@@ -0,0 +1,19 @@
+holiday = input("Enter your favorite Holiday: ")
+noun = input("Enter Noun: ")
+place = input("Enter Place: ")
+person = input("Enter Person")
+adjective = input("Enter adjective: ")
+body_part_plural = input("Enter Body Part Plural: ")
+verb = input("Enter Verb: ")
+food = input("Enter Food here: ")
+food2 = input("Enter Second Food here: ")
+plural_noun = input("Enter Plural Noun: ")
+famous_person = input("Enter Famous Person: ")
+family_member = input("Enter Family member: ")
+body_part = input("Enter body part: ")
+number = input("Enter Number: ")
+color = input("Enter a color: ")
+
+
+
+print(" I can't believe it's already " + holiday + " I can't wait to\n put on my " + noun + " and visit every " + place + " in my\n neighborhood. this year, I am going to dress up as a\n " + person + " with " + adjective + " " + body_part_plural + " Before I " + verb + " , I make sure to grab my " + adjective + " " + noun + " to hold all of\n my " + food + " Finally all of my " + plural_noun + " are ready to go!\n When " + famous_person+ " answers the door, i say, " + noun + " or\n Treat! Yum! I got a " + food + " and " + food2 + ". we\n visit " + number + " houses and decide its time to\n " + verb + " home. My " + family_member + " says if i eat too much " + food + ",\n my " + body_part + " will hurt, so i'll eat just " + number + ",\n pieces and go straight to bed. i hope i\'ll\n have " + adjective + " dreams of " + plural_noun + " tonight.\n Sky Night is " + color + " Happy " + holiday)
